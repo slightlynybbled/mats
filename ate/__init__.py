e92541ec53d136075577f24712e068516ab86dfd
Jason R. Jones <slightlynybbled@gmail.com>
2019-03-21 07:16:02 -0400

    Adding archiver to the __init__

--- ate/__init__.py
+++ ate/__init__.py
@@ -4,6 +4,7 @@ coloredlogs.install(level='DEBUG')
 
 from ate.test import Test
 from ate.test_sequence import TestSequence
+from ate.archiving import ArchiveManager
 from ate.version import __version__
 
-__all__ = ['Test', 'TestSequence', '__version__']
+__all__ = ['Test', 'TestSequence', 'ArchiveManager', '__version__']
