32c0f571de72adae4490c8399ad79339fc6263b8
Jason R. Jones <slightlynybbled@gmail.com>
2019-03-26 20:52:31 -0400

    Adding better examples

--- examples/production_test_burnin.py
+++ examples/production_test_burnin.py
@@ -23,37 +23,29 @@ class CommunicationTest(Test):
 
 
 # The FlowTest implements the `setup' and `teardown` methods as well in order to demonstrate what that may look like
-class FlowTest(Test):
+class BurnIn(Test):
     def __init__(self):
-        super().__init__(moniker='pump flow test',
-                         min_value=5.6, max_value=6.4)
+        super().__init__(moniker='burnin', min_value=5.6, max_value=6.4)
 
     def setup(self, aborted, is_passing):
-        # setting the speed of the pump might be something done in the setup, including
-        # the wait time to speed up the pump, which we will simulate with a 2s sleep
-        sleep(0.1)
+        # just wait for a while, maybe display a bit of a countdown...
+        seconds = 0
+        while seconds < 10:
+            seconds += 1
+            self._logger.info(f'burning in count: {seconds}s')
+            sleep(1.0)
 
     def execute(self, aborted, is_passing):
-        # user may abort the test based on the `aborted` or may
-        # continue the test, at the author's discretion
-        if aborted:
-            return None
-
-        # simulate long-running process, such as several flow measurement/averaging cycles
-        sleep(0.1)
-        flow = 5.5 + random()
+        # check to see if the device is still communicating
+        is_communicating = choice([True] * 3 + [False])
 
         # should return a (key, value) tuple which are the results of the test
-        return flow
-
-    def teardown(self, aborted, is_passing):
-        # again, simulating another long-running process...
-        sleep(0.1)
+        return is_communicating
 
 
 if __name__ == '__main__':
     # create the sequence of test objects
-    sequence = [CommunicationTest(), FlowTest()]
+    sequence = [CommunicationTest(), BurnIn()]
 
     # create the archive manager
     am = ArchiveManager()
@@ -64,6 +56,4 @@ if __name__ == '__main__':
                       auto_run=False)
 
     # start the test as many times as you wish!
-    for _ in range(3):
-        ts.start()
-        sleep(2.0)
+    ts.start()
