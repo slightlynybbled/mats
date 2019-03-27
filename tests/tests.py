c22d2f4733b88afec1547d74669925751adb4ce7
Jason R. Jones <slightlynybbled@gmail.com>
2019-03-27 08:27:00 -0400

    Adding good threshold testing

--- tests/tests.py
+++ tests/tests.py
@@ -16,6 +16,7 @@ def blank_Test():
 
     yield T()
 
+
 @pytest.fixture
 def pass_if_Test():
     class T(ate.Test):
@@ -44,6 +45,12 @@ def test_ate_version():
     assert ate.__version__
 
 
+def test_Test_execute_not_implemented():
+    t = ate.Test('test')
+    with pytest.raises(NotImplementedError):
+        t._execute(aborted=False, is_passing=True)
+
+
 def test_Test_creation(blank_Test):
     t = blank_Test
 
@@ -69,11 +76,6 @@ def test_Test_creation_with_min_and_max(bracketed_Test):
     assert t._max_value == 2.0
 
 
-def test_Test_execute_not_implemented():
-    t = ate.Test('test')
-    with pytest.raises(NotImplementedError):
-        t._execute(aborted=False, is_passing=True)
-
 def test_Test_blank_setup_execute_teardown(blank_Test):
     t = blank_Test
 
@@ -89,10 +91,98 @@ def test_Test_blank_setup_execute_teardown(blank_Test):
 
     t._execute(aborted=False, is_passing=True)
 
+    # check values after _exeecute()
     assert t.is_passing is True
     assert t.status == 'running'
 
     t._teardown(aborted=False, is_passing=True)
 
+    # check values after _teardown()
     assert t.is_passing is True
     assert t.status == 'complete'
+
+
+def test_Test_pass_if_should_pass(pass_if_Test):
+    t = pass_if_Test
+
+    def execute(aborted, is_passing):
+        return True
+
+    # overwrite the `execute` method with the method above
+    t.execute = execute
+
+    t._setup(aborted=False, is_passing=True)
+    t._execute(aborted=False, is_passing=True)
+
+    assert t.is_passing is True
+
+
+def test_Test_pass_if_should_fail(pass_if_Test):
+    t = pass_if_Test
+
+    def execute(aborted, is_passing):
+        return False
+
+    # overwrite the `execute` method with the method above
+    t.execute = execute
+
+    t._setup(aborted=False, is_passing=True)
+    t._execute(aborted=False, is_passing=True)
+
+    assert t.is_passing is False
+
+
+def test_Test_min_should_pass(bracketed_Test):
+    t = bracketed_Test
+
+    def execute(aborted, is_passing):
+        return 1.000001
+
+    t.execute = execute
+
+    t._setup(aborted=False, is_passing=True)
+    t._execute(aborted=False, is_passing=True)
+
+    assert t.is_passing is True
+
+
+def test_Test_min_should_fail(bracketed_Test):
+    t = bracketed_Test
+
+    def execute(aborted, is_passing):
+        return 0.999999
+
+    t.execute = execute
+
+    t._setup(aborted=False, is_passing=True)
+    t._execute(aborted=False, is_passing=True)
+
+    assert t.is_passing is False
+
+
+def test_Test_max_should_pass(bracketed_Test):
+    t = bracketed_Test
+
+    def execute(aborted, is_passing):
+        return 1.99999
+
+    t.execute = execute
+
+    t._setup(aborted=False, is_passing=True)
+    t._execute(aborted=False, is_passing=True)
+
+    assert t.is_passing is True
+
+
+def test_Test_max_should_fail(bracketed_Test):
+    t = bracketed_Test
+
+    def execute(aborted, is_passing):
+        return 2.00001
+
+    t.execute = execute
+
+    t._setup(aborted=False, is_passing=True)
+    t._execute(aborted=False, is_passing=True)
+
+    assert t.is_passing is False
