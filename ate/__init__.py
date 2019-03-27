ea48eab944cfa0d2aac6fac1eda07e794f72404e
Jason R. Jones <slightlynybbled@gmail.com>
2019-03-27 09:24:50 -0400

    Moving order of the coloredlogs installation step

--- ate/__init__.py
+++ ate/__init__.py
@@ -1,10 +1,10 @@
 import coloredlogs
 
-coloredlogs.install(level='DEBUG')
-
 from ate.test import Test
 from ate.test_sequence import TestSequence
 from ate.archiving import ArchiveManager
 from ate.version import __version__
 
 __all__ = ['Test', 'TestSequence', 'ArchiveManager', '__version__']
+
+coloredlogs.install(level='DEBUG')
