b1f336b315d4cd6ee33e007c78a0bfba58e8243a
Jason R. Jones <slightlynybbled@gmail.com>
2019-03-27 09:10:16 -0400

    Making file flake8 compatible - no change to function

--- ate/archiving.py
+++ ate/archiving.py
@@ -6,11 +6,16 @@ from shutil import copy
 
 
 class ArchiveManager:
-    def __init__(self, path='.', fname='data', fextension='.csv', delimiter='\t', loglevel=logging.INFO):
+    def __init__(self,
+                 path='.', fname='data', fextension='.csv',
+                 delimiter='\t', loglevel=logging.INFO):
         """
-        The basic save utility bundled into the test sequencer.  The archive manager is geared towards common manufacturing environments in which tab-delimited text files are common.
+        The basic save utility bundled into the test sequencer.  The archive \
+        manager is geared towards common manufacturing environments in which \
+        tab-delimited text files are common.
 
-        :param path: the path on which to save the data, not including the file name
+        :param path: the path on which to save the data, not including the \
+        file name
         :param fname: the file name
         :param fextension: the file extension
         :param delimiter: the delimiter or separator between fields
@@ -26,19 +31,31 @@ class ArchiveManager:
 
     def save(self, point: dict):
         """
-        Takes a point of data supplied as a dict and, depending on existing conditions, will archive the data point on the disk.  Three conditions possible at save time:
+        Takes a point of data supplied as a dict and, depending on existing
+        conditions, will archive the data point on the disk.  Three conditions
+        possible at save time:
 
          * Data file does not exist
-         * Data file exists and is compatible with the current data point (the header strings match)
-         * Data file exists, but is not compatible with the current data point (the header strings do not match)
+         * Data file exists and is compatible with the current data point \
+         (the header strings match)
+         * Data file exists, but is not compatible with the current data \
+         point (the header strings do not match)
 
-        When the data file does not exist, then the ``save()`` method will create the new data file at ``<fname>.<extension>``.
+        When the data file does not exist, then the ``save()`` method will
+        create the new data file at ``<fname>.<extension>``.
 
-        When the data file exists at ``<fname>.<extension>`` and is compatible with the current data point, then the ``save()`` method will simply append the new data point to previous data in a tabular format.
+        When the data file exists at ``<fname>.<extension>`` and is compatible
+        with the current data point, then the ``save()`` method will simply
+        append the new data point to previous data in a tabular format.
 
-        Finally, when the current data point is deemed incompatible with the previous data, then the ``save()`` method will copy the old file to ``<fname>_<datetime>.<extension>`` and then create a new file at ``<fname>.<extension>`` under which data will be collected until a new format is once again detected.
+        Finally, when the current data point is deemed incompatible with the
+        previous data, then the ``save()`` method will copy the old file to
+        ``<fname>_<datetime>.<extension>`` and then create a new file at
+        ``<fname>.<extension>`` under which data will be collected until a new
+        format is once again detected.
 
-        :param point: a ``dict`` containing key: value pairs which specify the data to be saved
+        :param point: a ``dict`` containing key: value pairs which specify
+        the data to be saved
         :return: None
         """
         destination_path = self._path / (self._fname + self._fextension)
@@ -66,11 +83,15 @@ class ArchiveManager:
         # check for the header string
         header_changed = False
         try:
-            # if the header strings do not match, then set the 'create_new_file' flag
+            # if the header strings do not match, then set the
+            # 'create_new_file' flag
             with open(destination_path, 'r') as f:
                 old_header_string = f.readline()
                 if old_header_string != header_string:
-                    self._logger.info(f'header strings do not match, "{old_header_string.strip()}" vs "{header_string.strip()}"')
+                    self._logger.info(
+                        f'header strings do not match, '
+                        f'"{old_header_string.strip()}" vs '
+                        f'"{header_string.strip()}"')
                     header_changed = True
         except FileNotFoundError:
             # if the file is not found, then create a new file
@@ -82,7 +103,8 @@ class ArchiveManager:
             now = now.split('.')[0]
 
             new_path = self._path / (self._fname + now + self._fextension)
-            self._logger.info(f'attempting to copy "{destination_path}" to "{new_path}"...')
+            self._logger.info(f'attempting to copy "{destination_path}" '
+                              f'to "{new_path}"...')
             copy(destination_path, new_path)
             remove(destination_path)
 
@@ -116,7 +138,3 @@ if __name__ == '__main__':
 
     sleep(2.0)
     am.save(point={'aa': 1.0, 'b': 2.0, 'c': 3.0})
-    #am.save(point={'a': 1.0, 'b': 2.0, 'c': 3.0})
-    #am.save(point={'aa': 1.0, 'b': 2.0, 'c': 3.0})
-    #am.save(point={'a': 1.0, 'b': 2.0, 'c': 3.0})
-    #am.save(point={'aa': 1.0, 'b': 2.0, 'c': 3.0})
