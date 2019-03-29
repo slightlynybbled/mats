f8fe2f0d99ae35d74388b752c0388ea447f51f71
Jason R. Jones <slightlynybbled@gmail.com>
2019-03-28 22:13:33 -0400

    Adding more testing

--- tests/test_TestSequence.py
+++ tests/test_TestSequence.py
@@ -7,34 +7,37 @@ import pytest
 import ate
 
 
+class T_normal_1(ate.Test):
+    def __init__(self):
+        super().__init__('test 1')
+
+    def execute(self, is_passing):
+        sleep(0.2)
+        return None
+
+
+class T_normal_2(ate.Test):
+    def __init__(self):
+        super().__init__('test 2')
+
+    def execute(self, is_passing):
+        return None
+
+
+t1, t2 = T_normal_1(), T_normal_2()
+
+
 @pytest.fixture
 def normal_test_sequence():
-    class T1(ate.Test):
-        def __init__(self):
-            super().__init__('test 1')
-
-        def execute(self, is_passing):
-            sleep(0.2)
-            return None
-
-    class T2(ate.Test):
-        def __init__(self):
-            super().__init__('test 2')
-
-        def execute(self, is_passing):
-            return None
-
-    yield ate.TestSequence(
-        sequence=[
-            T1(), T2()
-        ]
-    )
+    yield ate.TestSequence(sequence=[t1, t2])
 
 
 def test_TestSequence_creation(normal_test_sequence):
     ts = normal_test_sequence
 
     assert ts.in_progress is False
+    assert len(ts.test_names) == 2
+    assert len(ts.tests) == 2
 
 
 def test_TestSequence_retrieve_by_moniker(normal_test_sequence):
@@ -43,8 +46,22 @@ def test_TestSequence_retrieve_by_moniker(normal_test_sequence):
     assert test.moniker == 'test 1'
 
 
+def test_TestSequence_retrieve_by_Test(normal_test_sequence):
+    ts = normal_test_sequence
+    test = ts[t1]
+    assert test.moniker == 'test 1'
+
+
+def test_TestSequence_retrieve_by_integer_should_error(normal_test_sequence):
+    ts = normal_test_sequence
+    with pytest.raises(TypeError):
+        return ts[0]
+
+
 def test_TestSequence_run(normal_test_sequence):
     ts = normal_test_sequence
+
+    assert ts.ready
     ts.start()
 
     # wait a small amount of time, ensure that the test sequence has
