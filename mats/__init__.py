50755e3a5e6f077217e2fc650f01f27ed1a7bf4b
Jason R. Jones <slightlynybbled@gmail.com>
2020-11-19 09:04:22 -0500

    Fixing to pass flake8

--- mats/__init__.py
+++ mats/__init__.py
@@ -8,7 +8,8 @@ from mats.test_sequence import TestSequence
 from mats.tkwidgets import MatsFrame
 from mats.version import __version__
 
-__all__ = ['Test', 'TestSequence', 'ArchiveManager', 'MatsFrame', '__version__']
+__all__ = ['Test', 'TestSequence',
+           'ArchiveManager', 'MatsFrame', '__version__']
 
 coloredlogs.install(level='DEBUG')
 
