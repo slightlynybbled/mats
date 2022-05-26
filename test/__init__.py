a712f8ab7a5fef561303e68bd68b2226243a519a
Jason R. Jones <slightlynybbled@gmail.com>
2022-05-26 02:14:10 -0400

    Remove GUI testing for now

--- test/__init__.py
+++ test/__init__.py
@@ -4,12 +4,12 @@ import pytest
 
 # running GUI testing appears to make pytest unstable; leaving
 # here until i can decide how to handle
-@pytest.fixture
-def root():
-    root_frame = tk.Tk()
-    yield root_frame
-
-    try:
-        root_frame.destroy()
-    except:
-        pass
+# @pytest.fixture
+# def root():
+#     root_frame = tk.Tk()
+#     yield root_frame
+#
+#     try:
+#         root_frame.destroy()
+#     except:
+#         pass
